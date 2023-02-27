2e8633776f111906baaa6c6483d82250fc7b8cb2
Otavio Fernandes <otaviof@gmail.com>
2023-02-27 19:18:48 +0100

    Improve Formatting

---
+++ install-cli.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# Installs the Tekton CLI (tkn) informed version.
+#
+
+shopt -s inherit_errexit
+set -eu -o pipefail
+
+source "$(dirname ${BASH_SOURCE[0]})/common.sh"
+
+readonly INPUT_CLI_VERSION="${INPUT_CLI_VERSION:-}"
+
+[[ -z "${INPUT_CLI_VERSION}" ]] &&
+	fail "INPUT_CLI_VERSION enviroment variable is not set!"
+
+# the version needs to be numeric only to compose the tarball name
+readonly CLI_SHORT_VERSION="${INPUT_CLI_VERSION//v/}"
+
+readonly CLI_TARBALL="tkn_${CLI_SHORT_VERSION}_Linux_x86_64.tar.gz"
+readonly CLI_HOST_PATH="tektoncd/cli/releases/download"
+readonly CLI_URL="https://github.com/${CLI_HOST_PATH}/${INPUT_CLI_VERSION}/${CLI_TARBALL}"
+
+readonly TMP_DIR="/tmp"
+readonly OUTPUT_DOCUMENT="${TMP_DIR}/${CLI_TARBALL}"
+
+phase "Installing Tekton CLI '${INPUT_CLI_VERSION}'"
+
+# making sure the previous download is removed
+[[ -f "${OUTPUT_DOCUMENT}" ]] && rm -fv ${OUTPUT_DOCUMENT}
+
+phase "Downloading Tekton CLI tarball '${CLI_URL}'"
+wget --quiet --output-document="${OUTPUT_DOCUMENT}" ${CLI_URL}
+tar -C ${TMP_DIR} -zxvpf ${OUTPUT_DOCUMENT} tkn
+
+phase "Installing the CLI"
+exec install --verbose --mode=0755 "${TMP_DIR}/tkn" /usr/local/bin
